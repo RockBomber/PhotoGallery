5862d7e94571b17aea648d5f49971e48c439b8d3
V.Kuznetsov <v.kuznetsov@voskhod.ru>
2018-03-31 16:11:39 +0300

    Using GSON

--- app/src/main/java/com/bignerdranch/android/photogallery/FlickrFetchr.java
+++ app/src/main/java/com/bignerdranch/android/photogallery/FlickrFetchr.java
@@ -3,9 +3,7 @@ package com.bignerdranch.android.photogallery;
 import android.net.Uri;
 import android.util.Log;
 
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
+import com.google.gson.Gson;
 
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
@@ -63,34 +61,203 @@ public class FlickrFetchr {
                     .build().toString();
             String jsonString = getUrlString(url);
             Log.i(TAG, "Received JSON: " + jsonString);
-            JSONObject jsonBody = new JSONObject(jsonString);
-            parseItems(items, jsonBody);
+            Response response = new Gson().fromJson(jsonString, Response.class);
+            parseItems(items, response);
         } catch (IOException ioe) {
             Log.e(TAG, "Failed to fetch items", ioe);
-        } catch (JSONException je) {
-            Log.e(TAG, "Failed to parse JSON", je);
         }
 
         return items;
     }
 
-    private void parseItems(List<GalleryItem> items, JSONObject jsonBody) throws IOException, JSONException {
-        JSONObject photosJsonObject = jsonBody.getJSONObject("photos");
-        JSONArray photoJsonArray = photosJsonObject.getJSONArray("photo");
+    private void parseItems(List<GalleryItem> items, Response response) {
+        List<Photo> photos = response.getPhotos().getPhoto();
 
-        for (int i = 0; i < photoJsonArray.length(); i++) {
-            JSONObject photoJsonObject = photoJsonArray.getJSONObject(i);
+        for (Photo photo : photos) {
 
             GalleryItem item = new GalleryItem();
-            item.setId(photoJsonObject.getString("id"));
-            item.setCaption(photoJsonObject.getString("title"));
+            item.setId(photo.getId());
+            item.setCaption(photo.getTitle());
 
-            if (!photoJsonObject.has("url_s")) {
+            if (photo.getUrl_s() == null) {
                 continue;
             }
 
-            item.setUrl(photoJsonObject.getString("url_s"));
+            item.setUrl(photo.getUrl_s());
             items.add(item);
+        }
+    }
+
+    private class Response {
+
+        private Photos photos;
+
+        public Photos getPhotos() {
+            return photos;
+        }
+
+        public void setPhotos(Photos photos) {
+            this.photos = photos;
+        }
+    }
+
+    private class Photos {
+
+        private int page;
+        private int pages;
+        private int perpage;
+        private int total;
+        private List<Photo> photo;
+
+        public int getPage() {
+            return page;
+        }
+
+        public void setPage(int page) {
+            this.page = page;
+        }
+
+        public int getPages() {
+            return pages;
+        }
+
+        public void setPages(int pages) {
+            this.pages = pages;
+        }
+
+        public int getPerpage() {
+            return perpage;
+        }
+
+        public void setPerpage(int perpage) {
+            this.perpage = perpage;
+        }
+
+        public int getTotal() {
+            return total;
+        }
+
+        public void setTotal(int total) {
+            this.total = total;
+        }
+
+        public List<Photo> getPhoto() {
+            return photo;
+        }
+
+        public void setPhoto(List<Photo> photo) {
+            this.photo = photo;
+        }
+    }
+
+    private class Photo {
+        private String id;
+        private String owner;
+        private String secert;
+        private String server;
+        private int farm;
+        private String title;
+        private int ispublic;
+        private int isfriend;
+        private int isfamily;
+        private String url_s;
+        private String height_s;
+        private String width_s;
+
+        public String getId() {
+            return id;
+        }
+
+        public void setId(String id) {
+            this.id = id;
+        }
+
+        public String getOwner() {
+            return owner;
+        }
+
+        public void setOwner(String owner) {
+            this.owner = owner;
+        }
+
+        public String getSecert() {
+            return secert;
+        }
+
+        public void setSecert(String secert) {
+            this.secert = secert;
+        }
+
+        public String getServer() {
+            return server;
+        }
+
+        public void setServer(String server) {
+            this.server = server;
+        }
+
+        public int getFarm() {
+            return farm;
+        }
+
+        public void setFarm(int farm) {
+            this.farm = farm;
+        }
+
+        public String getTitle() {
+            return title;
+        }
+
+        public void setTitle(String title) {
+            this.title = title;
+        }
+
+        public int getIspublic() {
+            return ispublic;
+        }
+
+        public void setIspublic(int ispublic) {
+            this.ispublic = ispublic;
+        }
+
+        public int getIsfriend() {
+            return isfriend;
+        }
+
+        public void setIsfriend(int isfriend) {
+            this.isfriend = isfriend;
+        }
+
+        public int getIsfamily() {
+            return isfamily;
+        }
+
+        public void setIsfamily(int isfamily) {
+            this.isfamily = isfamily;
+        }
+
+        public String getUrl_s() {
+            return url_s;
+        }
+
+        public void setUrl_s(String url_s) {
+            this.url_s = url_s;
+        }
+
+        public String getHeight_s() {
+            return height_s;
+        }
+
+        public void setHeight_s(String height_s) {
+            this.height_s = height_s;
+        }
+
+        public String getWidth_s() {
+            return width_s;
+        }
+
+        public void setWidth_s(String width_s) {
+            this.width_s = width_s;
         }
     }
 
