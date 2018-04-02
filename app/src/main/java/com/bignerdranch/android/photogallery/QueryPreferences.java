1b446855906fb2846aafa56b4b13e736ea6630dd
V.Kuznetsov <v.kuznetsov@voskhod.ru>
2018-04-02 11:05:50 +0300

    Get new search result in service

--- app/src/main/java/com/bignerdranch/android/photogallery/QueryPreferences.java
+++ app/src/main/java/com/bignerdranch/android/photogallery/QueryPreferences.java
@@ -5,7 +5,8 @@ import android.preference.PreferenceManager;
 
 public class QueryPreferences {
 
-    private static String PREF_SEARCH_QUERY = "searchQuery";
+    private static final String PREF_SEARCH_QUERY = "searchQuery";
+    private static final String PREF_LAST_RESULT_ID = "lastResultId";
 
     public static String getStoredQuery(Context context) {
         return PreferenceManager.getDefaultSharedPreferences(context).getString(PREF_SEARCH_QUERY, null);
@@ -13,6 +14,14 @@ public class QueryPreferences {
 
     public static void setStoredQuery(Context context, String query) {
         PreferenceManager.getDefaultSharedPreferences(context).edit().putString(PREF_SEARCH_QUERY, query).apply();
+    }
+
+    public static String getLastResultId(Context context) {
+        return PreferenceManager.getDefaultSharedPreferences(context).getString(PREF_LAST_RESULT_ID, null);
+    }
+
+    public static void setLastResultId(Context context, String lastResultId) {
+        PreferenceManager.getDefaultSharedPreferences(context).edit().putString(PREF_LAST_RESULT_ID, lastResultId).apply();
     }
 
 }
