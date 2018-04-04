dbe12d331f0941ea950c5f1d33ef16ecedfed492
V.Kuznetsov <v.kuznetsov@voskhod.ru>
2018-04-04 11:31:31 +0300

    First ver working on API 25
    Needs improve for API 26
    
    This reverts commit c3d3b9bd9be1003bf83ae772e34bcf0c584c3954.

--- app/src/main/java/com/bignerdranch/android/photogallery/PollService.java
+++ app/src/main/java/com/bignerdranch/android/photogallery/PollService.java
@@ -1,17 +1,13 @@
 package com.bignerdranch.android.photogallery;
 
-import android.app.Activity;
 import android.app.AlarmManager;
 import android.app.IntentService;
 import android.app.Notification;
-import android.app.NotificationChannel;
-import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
 import android.content.res.Resources;
 import android.net.ConnectivityManager;
-import android.os.Build;
 import android.os.SystemClock;
 import android.support.v4.app.NotificationCompat;
 import android.support.v4.app.NotificationManagerCompat;
@@ -23,10 +19,6 @@ import java.util.concurrent.TimeUnit;
 public class PollService extends IntentService {
 
     private static final String TAG = "PollService";
-    public static final String ACTION_SHOW_NOTIFICATION = "com.bignerdranch.android.photogallery.SHOW_NOTIFICATION";
-    public static final String PERM_PRIVATE = "com.bekabot.android.photogallery.PRIVATE";
-    public static final String REQUEST_CODE = "REQUEST_CODE";
-    public static final String NOTIFICATION = "NOTIFICATION";
 
     // 60 seconds
     private static final long POLL_INTERVAL_MS = TimeUnit.MINUTES.toMillis(1);
@@ -81,48 +73,25 @@ public class PollService extends IntentService {
             Log.i(TAG, "Got an old result: " + resultId);
         } else {
             Log.i(TAG, "Got a new result: " + resultId);
-            String CHANNEL_ID = "channel_1";
 
             Resources resources = getResources();
             Intent i = PhotoGalleryActivity.newIntent(this);
             PendingIntent pi = PendingIntent.getActivity(this, 0, i, 0);
 
-            NotificationManager mNotificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
-
-            NotificationChannel mChannel;
-            int importance = NotificationManager.IMPORTANCE_DEFAULT;
-
-            NotificationCompat.Builder builder = new NotificationCompat.Builder(this, TAG)
+            Notification notification = new NotificationCompat.Builder(this)
                     .setTicker(resources.getString(R.string.new_pictures_text))
                     .setSmallIcon(android.R.drawable.ic_menu_report_image)
                     .setContentTitle(resources.getString(R.string.new_pictures_title))
                     .setContentText(resources.getString(R.string.new_pictures_text))
-                    .setContentIntent(pi);
+                    .setContentIntent(pi)
+                    .setAutoCancel(true)
+                    .build();
 
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-                mChannel = new NotificationChannel(CHANNEL_ID, this.getString(R.string.app_name), importance);
-                // Configure the notification channel.
-                mChannel.setDescription("notification");
-                mNotificationManager.createNotificationChannel(mChannel);
-            } else {
-                builder.setContentTitle(resources.getString(R.string.new_pictures_title))
-                        .setPriority(NotificationCompat.PRIORITY_DEFAULT);
-            }
-
-            builder.setAutoCancel(true);
-            builder.setChannelId(CHANNEL_ID);
-            Notification notification = builder.build();
-            showBackgroundNotification(0, notification);
+            NotificationManagerCompat notificationManager = NotificationManagerCompat.from(this);
+            notificationManager.notify(0, notification);
         }
 
         QueryPreferences.setLastResultId(this, resultId);
-    }
-
-    private void showBackgroundNotification(int requestCode, Notification notification) {
-        Intent i = new Intent(ACTION_SHOW_NOTIFICATION);
-        i.putExtra(REQUEST_CODE, requestCode);
-        i.putExtra(NOTIFICATION, notification);
-        sendOrderedBroadcast(i, PERM_PRIVATE, null, null, Activity.RESULT_OK, null, null);
     }
 
     private boolean isNetworkAvailableAndConnected() {
